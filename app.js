189870e591b7bc1151ef280292e5f63610c70079
Dhruvin3103 <dcchawda2003@gmail.com>
2022-11-04 11:40:04 +0530

    dhruvin ne kiya hai

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const prod=require('../E-Commerce/Routes/Product')
 const app=express()
 require('../E-Commerce/dbConnect')
 app.use(express.json())
-app.use(morgan('dev'))
+app.use(morgan('tiny'))
 app.use('/user',user)
 app.use('/product',prod)
 app.use((req,res,next)=>{
